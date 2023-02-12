a7ec1e4c81def96ec09d55b2e2bede124564df71
hugocth <hugo.cth@gmail.com>
2023-02-12 22:18:52 +0800

    Added model size computation

--- experiments_quantization.py
+++ experiments_quantization.py
@@ -79,7 +79,7 @@ def load_dataset(dataset, model_is_LeNet=False):
     return trainloader, testloader, in_channels, num_classes
 
 
-def train(model, trainloader, testloader, is_double_experiment=False):
+def train(model, trainloader, is_double_experiment=False):
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
@@ -134,15 +134,27 @@ def test(model, testloader, is_double_experiment=False):
     accuracy = 100 * correct / total
     inference_time = t1 - t0
 
+    def get_model_size(model):
+        param_size = 0
+        for param in model.parameters():
+            param_size += param.nelement() * param.element_size()
+        buffer_size = 0
+        for buffer in model.buffers():
+            buffer_size += buffer.nelement() * buffer.element_size()
+        size_all_mb = (param_size + buffer_size) / 1024**2
+        return round(size_all_mb, 2)
+    
+    model_size = get_model_size(model)
+
     print('Accuracy of the network on the 10000 test images: %d %%' % (accuracy))
     print(f'Elapsed time for inference: {int(inference_time)}s')
 
-    return accuracy, inference_time
+    return accuracy, inference_time, model_size
 
 
 def main(datasets):
 
-    res_df = pd.DataFrame(columns=["Model", "Dataset", "Quantization", "Accuracy", "Training Time", "Inference Time"])
+    res_df = pd.DataFrame(columns=["Model", "Dataset", "Quantization", "Accuracy", "Training Time", "Inference Time", "Model Size"])
 
     for dataset in datasets:
         ## Load dataset
@@ -168,8 +180,8 @@ def main(datasets):
 
             ## fp32
             model, training_time = train(model=model, trainloader=trainloader) ## Simple precision
-            accuracy, inference_time = test(model=model, testloader=testloader)
-            res_df.loc[res_df.shape[0]] = [models_labels[i], dataset, torch.float, accuracy, training_time, inference_time]
+            accuracy, inference_time, model_size = test(model=model, testloader=testloader)
+            res_df.loc[res_df.shape[0]] = [models_labels[i], dataset, torch.float, accuracy, training_time, inference_time, model_size]
 
             ## float16 and int8 --> doesn't work on macOS ...
             for quantization in [torch.float16, torch.qint8]:
@@ -178,12 +190,12 @@ def main(datasets):
                     {torch.nn.Conv2d, torch.nn.Linear},     # a set of layers to dynamically quantize
                     dtype=quantization                      # the target dtype for quantized weights
                 )
-                accuracy, inference_time = test(model=q_model, testloader=testloader)
-                res_df.loc[res_df.shape[0]] = [models_labels[i], dataset, quantization, accuracy, training_time, inference_time]
+                accuracy, inference_time, model_size = test(model=q_model, testloader=testloader)
+                res_df.loc[res_df.shape[0]] = [models_labels[i], dataset, quantization, accuracy, training_time, inference_time, model_size]
 
             ## fp64
-            accuracy, training_time = test(model=model.double(), testloader=testloader, is_double_experiment=True)
-            res_df.loc[res_df.shape[0]] = [models_labels[i], dataset, torch.double, accuracy, training_time, inference_time]
+            accuracy, training_time, model_size = test(model=model.double(), testloader=testloader, is_double_experiment=True)
+            res_df.loc[res_df.shape[0]] = [models_labels[i], dataset, torch.double, accuracy, training_time, inference_time, model_size]
 
     now = datetime.now()
     dt_string = now.strftime("%d-%m-%Y %H:%M:%S")
