c46e409c9f74994faa07ad6b5725876a90b62919
hugocth <hugo.cth@gmail.com>
2023-02-17 15:47:30 +0800

    Modified bench

--- experiments_quantization.py
+++ experiments_quantization.py
@@ -4,18 +4,46 @@
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-
-import torchvision
-import torchvision.transforms as transforms
 
 import pandas as pd
 import time
 from datetime import datetime
+import os
+from copy import deepcopy
 
 from networks import LeNet, AlexNet, VGG11, BasicBlock, ResNet
 from dataset_downloaders import load_dataset
+
+def quantization_experiment(model, model_label, dataset, test_loader):
+    data_types = ["float32", torch.float16 torch.qint8, "float64"]
+    res = []
+    for data_type in data_types:
+        print(f"Begin for {model_label}, {data_type}")
+        if data_type == "float32":
+            model = model
+            accuracy, mag, inference_time = test(model=model, testloader=test_loader)
+            model_size = get_size_of_model(model)
+        elif data_type == "float64":
+            d_model = model.double()
+            accuracy, mag, inference_time = test(model=d_model, testloader=test_loader, is_double_experiment=True)
+            model_size = get_size_of_model(model)            
+        else:
+            q_model = torch.quantization.quantize_dynamic(
+                model, 
+                {nn.Conv2d, nn.Linear}, 
+                dtype=data_type) 
+            accuracy, mag, inference_time = test(model=q_model, testloader=test_loader)
+            model_size = get_size_of_model(model)                  
+        res.append([model_label, dataset, data_type, accuracy, mag, inference_time, model_size])
+    return res
+
+
+def get_size_of_model(model, label=""):
+    torch.save(model.state_dict(), "temp.p")
+    size=os.path.getsize("temp.p")
+    print("model: ",label,' \t','Size (KB):', size/1e3)
+    os.remove('temp.p')
+    return size
 
 
 def test(model, testloader, is_double_experiment=False): # TODO: modify to quantisize model AND inputs, and many quantization modes (dynamic, static, QAT ...)
@@ -35,74 +63,49 @@ def test(model, testloader, is_double_experiment=False): # TODO: modify to quant
 
     accuracy = 100 * correct / total
     inference_time = t1 - t0
-
-    def get_model_size(model):
-        param_size = 0
-        for param in model.parameters():
-            param_size += param.nelement() * param.element_size()
-        buffer_size = 0
-        for buffer in model.buffers():
-            buffer_size += buffer.nelement() * buffer.element_size()
-        size_all_mb = (param_size + buffer_size) / 1024**2
-        return round(size_all_mb, 2)
-    
-    model_size = get_model_size(model)
+    mag = torch.mean(abs(outputs)).item()
 
     print('Accuracy of the network on the 10000 test images: %d %%' % (accuracy))
-    print(f'Elapsed time for inference: {int(inference_time)}s')
-    print(f"Model Size: {model_size} MB")
+    print(f'Elapsed time for inference: {inference_time}s')
 
-    return accuracy, inference_time, model_size
+    return accuracy, mag, inference_time
 
 
 def main():
 
-    res_df = pd.DataFrame(columns=["Model", "Dataset", "Quantization", "Accuracy", "Training Time", "Inference Time", "Model Size"])
+    res_df = pd.DataFrame(columns=["Model", "Dataset", "Quantization", "Prediction Accuracy", "Mean Abs Value of Output", "Inference Time", "Model Size"])
 
-    for dataset in datasets:
-        ## Load dataset
-        trainloader, testloader, in_channels, num_classes = load_dataset(dataset)
+    trained_model_filenames= [model_filename for model_filename in os.listdir(path="models") if not model_filename[0]=="."]
+    for trained_model_filename in trained_model_filenames:
 
-        ## Create instances of models
-        leNet = LeNet(in_channels=in_channels, num_classes=num_classes)
-        alexNet = AlexNet(in_channels=in_channels, num_classes=num_classes)
-        vgg11 = VGG11(in_channels=in_channels, num_classes=num_classes)
-        resNet18 = ResNet(in_channels=in_channels, num_layers=18, block=BasicBlock, num_classes=num_classes)
-        models = [leNet, alexNet, vgg11, resNet18]
-        models_labels = ["leNet", "alexNet", "vgg11", "resNet18"]
+        PATH = f"models/{trained_model_filename}"
+        model_label = trained_model_filename.split("_")[0]
+        dataset = trained_model_filename.split("_")[2]
         
-        ## For local test
-        models = [leNet]
-        models_labels = ["leNet"]
+        # To test on only 1 model and 1 dataset
+        if not (model_label == "alexNet" & dataset =="MNIST"):
+            continue
+
+        flag = (model_label == "leNet")
+        _, test_loader, in_channels, num_classes = load_dataset(dataset=dataset, model_is_LeNet=flag)
+
+        device = torch.device('cpu')
+        assert model_label in ["leNet", "alexNet", "vgg11", "resNet18"]
+        if model_label == "leNet":
+            model = LeNet(in_channels=in_channels, num_classes=num_classes)
+        elif model_label == "alexNet":
+            model = AlexNet(in_channels=in_channels, num_classes=num_classes)
+        elif model_label == "vgg11":
+            model = VGG11(in_channels=in_channels, num_classes=num_classes)
+        elif model_label == "resNet18":
+            model = ResNet(in_channels=in_channels, num_layers=18, block=BasicBlock, num_classes=num_classes)
         
-
-        for i, model in enumerate(models):
-
-            if model == leNet:
-                trainloader, testloader, in_channels, num_classes = load_dataset(dataset, model_is_LeNet=True)
-
-            ## fp32
-            model, training_time = train(model=model, trainloader=trainloader) ## Simple precision
-            print("For fp32")
-            accuracy, inference_time, model_size = test(model=model, testloader=testloader)
-            res_df.loc[res_df.shape[0]] = [models_labels[i], dataset, torch.float, accuracy, training_time, inference_time, model_size]
-
-            ## float16 and int8 --> doesn't work on macOS ...
-            for quantization in [torch.float16, torch.qint8]:
-                q_model = torch.quantization.quantize_dynamic(
-                    model,                                  # the original model
-                    {torch.nn.Conv2d, torch.nn.Linear},     # a set of layers to dynamically quantize
-                    dtype=quantization                      # the target dtype for quantized weights
-                )
-                print(f"For {quantization}")
-                accuracy, inference_time, model_size = test(model=q_model, testloader=testloader)
-                res_df.loc[res_df.shape[0]] = [models_labels[i], dataset, quantization, accuracy, training_time, inference_time, model_size]
-
-            ## fp64
-            print(f"For fp64")
-            accuracy, training_time, model_size = test(model=model.double(), testloader=testloader, is_double_experiment=True)
-            res_df.loc[res_df.shape[0]] = [models_labels[i], dataset, torch.double, accuracy, training_time, inference_time, model_size]
-
+        model.load_state_dict(torch.load(PATH, map_location=device))
+        print(f"Begin for {trained_model_filename}...")
+        experiment_results = quantization_experiment(model=model, model_label=model_label, dataset=dataset, test_loader=test_loader)
+        for result in experiment_results:
+            res_df.loc[res_df.shape[0]] = result
+        
     now = datetime.now()
     dt_string = now.strftime("%d-%m-%Y %H:%M:%S")
     res_df.to_csv(f"results/experiment_{dt_string}.csv")
@@ -110,5 +113,4 @@ def main():
 
 
 if __name__ == "__main__":
-    datasets = ["MNIST"]
-    main(datasets)
+    main()
